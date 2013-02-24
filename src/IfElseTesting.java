8b716fcadb555c0c528db66cafb2799d619ea2e9
Chase McDonald <thed3coder@gmail.com>
2013-02-24 08:59:58 +0000

    Added IfElseTesting.class Files to test if else statements

---
+++ src/IfElseTesting.java
@@ -0,0 +1,13 @@
+public class IfElseTesting {
+	
+	private static int x = 50;
+	private static int y = 20;
+public static void main(String args[]){
+		if(x == 10){
+			System.out.println("X == 10");
+		}
+		else{
+			System.out.println("X != 10");
+		}
+	}
+}
